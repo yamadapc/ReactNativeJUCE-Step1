769a09bc76f5f306ec5e7256bb4762d45a1c5d80
yamadapc <tacla.yamada@gmail.com>
2019-03-09 08:01:27 -0300

    Copy YUI renderer as a template

--- JavaScript/react-native-juce/src/index.js
+++ JavaScript/react-native-juce/src/index.js
@@ -28,13 +28,88 @@ class NativeModuleRegistry {
 global.modules = new NativeModuleRegistry();
 export const modules = global.modules;
 
+const rootHostContext = {};
+const childHostContext = {};
+
 const JUCERenderer = Reconciler({
+  now: Date.now,
+
+  getRootHostContext: () => {
+    return rootHostContext;
+  },
+
+  prepareForCommit: () => {},
+
+  resetAfterCommit: () => {},
+
+  getChildHostContext: () => {
+    return childHostContext;
+  },
+
+  shouldSetTextContent: (type, props) => {
+    return false;
+  },
+
+  createInstance: (type, newProps, rootContainerInstance, _currentHostContext, workInProgress) => {
+    const yueElement = gui.Container.create();
+    Object.keys(newProps).forEach(propName => {
+      const propValue = newProps[propName];
+      if (propName === 'children') {
+        if (type !== 'text') {
+          if (typeof propValue === 'string' || typeof propValue === 'number') {
+            throw new Error('Text strings must be rendered within a <Text> component.');
+          }
+
+          if (propValue instanceof Array) {
+            propValue.forEach(item => {
+              if (typeof item === 'string') {
+                throw new Error('Text strings must be rendered within a <Text> component.');
+              }
+            });
+          }
+        }
+      } else if (propName === 'style') {
+        yueElement.setStyle(propValue);
+      }
+    });
+    return yueElement;
+  },
+
+  createTextInstance: (text, rootContainerInstance, hostContext, internalInstanceHandle) => {
+    return gui.Label.create(text);
+  },
+
+  appendInitialChild: (parent, child) => {
+    parent.addChildView(child);
+  },
+
+  appendChild(parent, child) {
+    parent.addChildView(child);
+  },
+
+  finalizeInitialChildren: (yueElement, type, props) => {},
+
+  supportsMutation: true,
+
+  appendChildToContainer: (parent, child) => {
+    parent.setContentView(child);
+  },
+
+  prepareUpdate(yueElement, oldProps, newProps) {
+    return true;
+  },
+
+  commitUpdate(yueElement, updatePayload, type, oldProps, newProps) {},
+
+  commitTextUpdate(textInstance, oldText, newText) {},
+
+  removeChild(parentInstance, child) {}
 });
 
 export function render(element, container, callback) {
-  if (!container._rootContainer) {
-    container._rootContainer = JUCERenderer.createContainer(container, false);
-  }
+  // if (!container._rootContainer) {
+  //   container._rootContainer = JUCERenderer.createContainer(container, false);
+  // }
 
-  return JUCERenderer.updateContainer(element, container._rootContainer, null, callback);
+  return JUCERenderer.updateContainer(element, container, null, callback);
 }
